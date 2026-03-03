a9a71fb9deb0f05aa9bd87f7c69a9ff7841d30d8
ttl <bradaxelson@gmail.com>
2026-03-03 20:41:57 +0000

    chore: Function stubs

--- src/VendingMachine.sol
+++ src/VendingMachine.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.13;
 
 import "./IVendingMachine.sol";
 
-contract VendingMachine is IVendingMachine {}
+contract VendingMachine is IVendingMachine {
+    function addInventory(string memory location, uint128 price, uint64 stock) external {}
+    function removeInventory(string memory location) external {}
+    function restock(string memory location, uint64 stock) external {}
+    function reprice(string memory location, uint128 price) external {}
+    function purchase(string memory location) external payable {}
+    function pause() external {}
+    function unpause() external {}
+    function collect() external {}
+}
